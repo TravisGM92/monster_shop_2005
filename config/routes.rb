7e2f7c9dc0d5f6753aca2cb9aa5aa71c77b7029a
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-10 10:38:19 -0600

    Refactored the routes a little

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "welcome#index"
   resources :merchants, only: [:index, :new, :show]
-  # get "/merchants", to: "merchants#index"
-  # get "/merchants/new", to: "merchants#new"
-  # get "/merchants/:id", to: "merchants#show"
   post "/merchants", to: "merchants#create"
   get "/merchants/:id/edit", to: "merchants#edit"
   patch "/merchants/:id", to: "merchants#update"
@@ -12,8 +9,7 @@ Rails.application.routes.draw do
 
 
   resources :items, only: [:index, :show]
-  # get "/items", to: "items#index"
-  # get "/items/:id", to: "items#show"
+
   get "/items/:id/edit", to: "items#edit"
   patch "/items/:id", to: "items#update"
   get "/merchants/:merchant_id/items", to: "items#index"
