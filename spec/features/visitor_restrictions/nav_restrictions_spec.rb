a06e87d5c06436e70d4c28c70541800c9ee103b5
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-09 19:14:47 -0600

    Write tests and pass US 7

--- spec/features/visitor_restrictions/nav_restrictions_spec.rb
+++ spec/features/visitor_restrictions/nav_restrictions_spec.rb
@@ -27,15 +27,24 @@ RSpec.describe 'Site Navigation Restrictions' do
     describe "When I try to access any path that begins with the following" do
       it "I see a 404 error (/merchant, /admin)" do
 
-        # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        user = User.create(name: 'Ross Mooney',
+                              address: '2468 S. Grant St.',
+                              city: 'Littleton',
+                              state: 'CO',
+                              zip: '80121',
+                              email: 'ross_is_cool@turing.io',
+                              password: 'test124',
+                              role: 0)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
         visit '/merchant'
 
-        # expect(page).to eq("The page you were looking for doesn't exist.")
+        expect(page).to have_content("The page you were looking for doesn't exist.")
 
-        # visit '/admin'
+        visit '/admin'
 
-        # expect(page).to eq("The page you were looking for doesn't exist.")
+        expect(page).to have_content("The page you were looking for doesn't exist.")
       end
     end
   end
