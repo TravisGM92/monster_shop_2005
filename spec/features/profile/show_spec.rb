040432813fddb9b3e71a60173c4e83f340ee4dbe
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-10 11:24:18 -0600

    Add another spec to the test to make sure password isn't being displayed

--- spec/features/profile/show_spec.rb
+++ spec/features/profile/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Profile show' do
     describe 'Then I see all of my profile data on the page except my password' do
       describe "And I see a link to edit my profile data" do
         before(:each) do
-          ross = User.create(name: 'Ross Mooney',
+          @ross = User.create!(name: 'Ross Mooney',
             address: '2468 S. Grant St.',
             city: 'Littleton',
             state: 'CO',
@@ -13,11 +13,24 @@ RSpec.describe 'Profile show' do
             email: 'ross_is_cool@turing.io',
             password: 'test124',
             role: 0)
-            
+
           end
 
-          it 'I see all items Ive added to my cart' do
-            visit '/cart'
+          it 'I see all of my information' do
+            allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@ross)
+
+            visit "/profile"
+
+            expect(page).to have_content("#{@ross.name}")
+            expect(page).to have_content("#{@ross.address}")
+            expect(page).to have_content("#{@ross.city}")
+            expect(page).to have_content("#{@ross.state}")
+            expect(page).to have_content("#{@ross.zip}")
+            expect(page).to have_content("#{@ross.email}")
+            expect(page).to have_content("Role: default")
+            expect(page).to_not have_content("#{@ross.password}")
+
+            expect(page).to have_link("Edit profile")
 
           end
         end
