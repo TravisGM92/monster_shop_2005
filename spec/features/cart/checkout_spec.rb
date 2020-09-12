59f1e89f246763cb6621f9d90354934ae71c48d1
Corey Carter <crc2231@gmail.com>
2020-09-11 20:32:55 -0600

    Add spec to remove one from quantity and remove item from cart if quantity == 0

--- spec/features/cart/checkout_spec.rb
+++ spec/features/cart/checkout_spec.rb
@@ -54,6 +54,48 @@ RSpec.describe 'Cart show' do
           expect(page).to have_content("2")
         end
       end
+
+      it 'Can incrementally decrease quantity' do
+        @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+        @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+        @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+        @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+        @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+        
+        visit "/items/#{@tire.id}"
+        click_on "Add To Cart"
+        visit "/items/#{@tire.id}"
+        click_on "Add To Cart"
+        
+        visit "/cart"
+        
+        within "#cart-item-#{@tire.id}" do
+          expect(page).to have_content("2")
+          click_button "Put One Back!"
+          expect(page).to have_content("1")
+        end
+      end
+
+      it 'will remove item from cart if quantity decreased to 0' do
+        @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+        @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+        @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+        @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+        @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+        
+        visit "/items/#{@tire.id}"
+        click_on "Add To Cart"
+        
+        visit "/cart"
+        
+        within "#cart-item-#{@tire.id}" do
+          expect(page).to have_content("1")
+          click_button "Put One Back!"
+        end
+        expect(page).to_not have_content("#{@tire.name}")
+      end
     end
 
   describe 'When I havent added items to my cart' do
