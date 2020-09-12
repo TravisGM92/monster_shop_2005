907782c5e53acca9924a50e4652c8ce08afd90c7
Corey Carter <crc2231@gmail.com>
2020-09-12 17:18:56 -0600

    Remove duplicate routes

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,6 @@ Rails.application.routes.draw do
   post "/profile/update", to: 'users#update'
   get '/profile/edit_password', to: 'users#password_update'
   post '/profile/edit_password', to: 'users#new_password'
-  get '/profile/orders', to: 'orders#index'
   get '/profile/orders/:id', to: 'orders#show'
 
   namespace :merchant do
