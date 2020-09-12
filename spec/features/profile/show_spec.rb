2b623c57c8f9ddb626ccb8088f4d16e5600c1fb2
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-12 15:36:36 -0600

    Create test for US 27

--- spec/features/profile/show_spec.rb
+++ spec/features/profile/show_spec.rb
@@ -76,8 +76,6 @@ RSpec.describe 'Profile show' do
                           expect(page).to have_content("Address: homeless\n")
                           expect(page).to_not have_content("Ross")
 
-
-
                         end
                       end
                     end
@@ -86,8 +84,46 @@ RSpec.describe 'Profile show' do
               end
             end
           end
-        end
+          describe "If I have orders placed, I see a  link on my profile page called 'My Orders'"
+          it "when I click this link my URI path is '/profile/orders'" do
+            @mike = Merchant.create!(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+            @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+            @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+            @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+            @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
 
+              allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@ross)
+
+
+              visit "/items/#{@paper.id}"
+              click_on "Add To Cart"
+              visit "/items/#{@paper.id}"
+              click_on "Add To Cart"
+              visit "/items/#{@tire.id}"
+              click_on "Add To Cart"
+              visit "/items/#{@pencil.id}"
+              click_on "Add To Cart"
+
+              visit "/cart"
+
+              click_on "Checkout"
+
+              visit "/cart"
+              click_on "Checkout"
+
+              fill_in :name, with: "Billy"
+              fill_in :address, with: "homeless"
+              fill_in :city, with: "Aurora"
+              fill_in :state, with: "CO"
+              fill_in :zip, with: "80226"
+              click_button("Create Order")
+              visit("/profile")
+
+              expect(page).to have_link("My Orders")
+              click_link("My Orders")
+              expect(current_path).to eq("/profile/orders")
+            end
+          end
+        end
       end
     end
-  end
