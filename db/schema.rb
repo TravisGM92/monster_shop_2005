436c31f92d22e4a2ea3c1042a5ea9dcea67bd0a2
Saryn Mooney <smooney15@my.whitworth.edu>
2020-09-13 16:03:48 -0600

    us30 not finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_13_155832) do
+ActiveRecord::Schema.define(version: 2020_09_13_215105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2020_09_13_155832) do
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quantity"
     t.integer "quantity_purchased", default: 0
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
@@ -60,7 +59,7 @@ ActiveRecord::Schema.define(version: 2020_09_13_155832) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "status", default: 0
+    t.string "status"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -80,7 +79,7 @@ ActiveRecord::Schema.define(version: 2020_09_13_155832) do
     t.integer "zip"
     t.string "email"
     t.string "password_digest"
-    t.integer "role"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "item_orders", "items"
