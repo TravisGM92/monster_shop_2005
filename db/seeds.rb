025cbee7d412ac5d3a33b365d2c8991f7afca673
Corey Carter <crc2231@gmail.com>
2020-09-17 14:16:21 -0600

    Delete drop status from order migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,51 @@ bike = bike_shop.items.create(name: "Street Bike", description: "Brand new", pri
 #dog_shop items
 pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32, quantity_purchased: 20)
 dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21, quantity_purchased: 12)
+
+# @susan = User.create!(name: 'Susan Sarandon',
+#     address: '1235 Beverly Hills Blvd',
+#     city: 'Hollywood',
+#     state: 'CA',
+#     zip: '90210',
+#     email: 'actress@movies.com',
+#     password: 'password',
+#     role: 2)
+
+#     @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+#     @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+#     @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+#     @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+#     @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+
+#     @ross = User.create!(name: 'Ross Mooney',
+#     address: '2468 S. Grant St.',
+#     city: 'Littleton',
+#     state: 'CO',
+#     zip: '80121',
+#     email: 'ross_is_cool@turing.io',
+#     password: 'test124',
+#     role: 0)
+
+#     @dwight = User.create!(name: 'Dwight Shrute',
+#     address: '5423 Main St.',
+#     city: 'Scranton',
+#     state: 'PA',
+#     zip: '52341',
+#     email: 'assistant_to_the_Manager@dundermifflin.com',
+#     password: 'ihatejim',
+#     role: 0)
+
+#     @order_1 = @ross.orders.create(name: "Bert", address: "123 Sesame St.", city: "NYC", state: "New York", zip: 10001, status: 1)
+#     @io1 = @paper.item_orders.create(order_id: @order_1.id, quantity: 4, price: 20)
+#     @io2 = @tire.item_orders.create(order_id: @order_1.id, quantity: 2, price: 100)
+
+#     @order_2 = @ross.orders.create(name: "Ernie", address: "2343 Sesame St.", city: "NYC", state: "NY", zip: 55555, status: 0)
+#     @io3 = @paper.item_orders.create(order_id: @order_2.id, quantity: 5, price: 20)
+#     @io4 = @tire.item_orders.create(order_id: @order_2.id, quantity: 3, price: 100)
+#     @io5 = @pencil.item_orders.create(order_id: @order_2.id, quantity: 1, price: 2)
+
+#     @order_3 = @dwight.orders.create(name: "Michael", address: "2341 BestBoss Ave.", city: "Scranton", state: "PA", zip: 41241, status: 3)
+#     @io5 = @paper.item_orders.create(order_id: @order_3.id, quantity: 1000, price: 2)
+    
+#     @order_4 = @dwight.orders.create(name: "Jim", address: "6235 Kazoo St.", city: "Pittburgh", state: "PA", zip: 41241, status: 2)
+#     @io5 = @paper.item_orders.create(order_id: @order_3.id, quantity: 1000, price: 2)
